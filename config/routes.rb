ea527894c9090a51f50d815251d49074985a8ab0
Vishal Agarwal <vishal.agl17@gmail.com>
2022-10-28 16:12:07 +0530

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
+
   root "posts#index"
   resources :users
+
   get 'manage', to: 'users#manage'
+
+  resources :users, only: [:manage] do
+    member do
+      patch :ban
+    end
+  end
+
   resources :posts do
     resources :comments
   end
